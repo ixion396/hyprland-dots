20a93ffebdba408b42feb90c6db850207b125893
Corbin Wunderlich <corbin@wcopy.net>
2023-11-08 17:17:03 -0500

    half functioning ags bar, to be finished on new laptop

--- dot_config/ags/widgets/bar/bar.ts
+++ dot_config/ags/widgets/bar/bar.ts
@@ -1,9 +1,28 @@
 import Widget, {Button} from 'resource:///com/github/Aylur/ags/widget.js';
-import Battery from 'resource:///com/github/Aylur/ags/service/battery.js';
 import hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
 import * as Utils from "resource:///com/github/Aylur/ags/utils.js"
+import battery from 'resource:///com/github/Aylur/ags/service/battery.js';
+
+const launcher = () => Widget.Button ({
+    className: "launcher",
+    valign: "end",
+    
+    onClicked: () => Utils.execAsync("wofi --show drun"),
+
+    child: Widget.Label ("󰣇")
+})
 
 const workspaceDispatch = ws => Utils.execAsync(`hyprctl dispatch workspace ${ws}`)
+
+function getWorkspaceLabel(workspaceId) {
+    if (workspaceId === 1) return ""
+    if (workspaceId === 2) return "󰈹"
+    if (workspaceId === 3) return "󰈙"
+    if (workspaceId === 4) return "󰅨"
+    if (workspaceId === 5) return "󱎓"
+    if (workspaceId === 6) return "󰉼"
+    return ""
+}
 
 const workspaces = () => Widget.EventBox ({
     className: "workspaces",
@@ -24,19 +43,19 @@ const workspaces = () => Widget.EventBox ({
         //     }));
         // }]],
 
-        children: Array.from({ length: 10 }, (_, i) => i + 1).map(i => Widget.EventBox ({
-            className: "workspace",
-            child: Widget.Label(" "),
-            setup: button => button.name = i.toString(),
-            onPrimaryClick: () => workspaceDispatch(i),
-        })),
-
-        // children: Array.from({ length: 10 }, (_, i) => i + 1).map(i => Widget.Button ({
+        // children: Array.from({ length: 10 }, (_, i) => i + 1).map(i => Widget.EventBox ({
         //     className: "workspace",
         //     child: Widget.Label(" "),
         //     setup: button => button.name = i.toString(),
-        //     onClicked: () => workspaceDispatch(i),
+        //     onPrimaryClick: () => workspaceDispatch(i),
         // })),
+
+        children: Array.from({ length: 10 }, (_, i) => i + 1).map(i => Widget.Button ({
+            className: "workspace",
+            child: Widget.Label(getWorkspaceLabel(i)),
+            setup: button => button.name = i.toString(),
+            onClicked: () => workspaceDispatch(i),
+        })),
 
         connections: [[hyprland, self => self.children.forEach(button => {
             button.visible = hyprland.workspaces.some(ws => ws.id === Number.parseInt(button.name))
@@ -45,16 +64,60 @@ const workspaces = () => Widget.EventBox ({
     })
 })
 
-const quickView = () => Widget.Box ({
+function getBatteryLabel() {
+    if (battery.charging) {
+        if (battery.percent >= 10) return "󰢜"
+        if (battery.percent >= 20) return "󰂆"
+        if (battery.percent >= 30) return "󰂇"
+        if (battery.percent >= 40) return "󰂈"
+        if (battery.percent >= 50) return "󰢝"
+        if (battery.percent >= 60) return "󰂉"
+        if (battery.percent >= 70) return "󰢞"
+        if (battery.percent >= 80) return "󰂊"
+        if (battery.percent >= 90) return "󰂋"
+        if (battery.percent === 100) return "󰂅"
+
+        return "󰢟"
+    }
+
+    if (battery.percent >= 10) return "󰁺"
+    if (battery.percent >= 20) return "󰁻"
+    if (battery.percent >= 30) return "󰁼"
+    if (battery.percent >= 40) return "󰁽"
+    if (battery.percent >= 50) return "󰁿"
+    if (battery.percent >= 60) return "󰁿"
+    if (battery.percent >= 70) return "󰂀"
+    if (battery.percent >= 80) return "󰂁"
+    if (battery.percent >= 90) return "󰂂"
+    if (battery.percent === 100) return "󰁹"
+
+    return "󰂎"
+}
+
+const quickView = () => Widget.Button ({
     className: "quick-view",
     valign: "start",
-    halign: "start",
-    vertical: true,
-    children: [
-        Widget.Label ({
-            
-        })
-    ]
+    child: Widget.Box ({
+        vertical: true,
+        children: [
+            Widget.Label ({
+                className: "battery",
+                halign: "center",
+                connections: [[battery, self => self.label = getBatteryLabel()]]
+            }),
+
+            Widget.Label ({
+
+            }),
+
+            Widget.Label ({
+                className: "clock",
+                halign: "center",
+                connections: [[1000, self => self.label = Utils.exec("date +%H%M").slice(0, 2) + "\n" + 
+                    Utils.exec("date +%H%M").slice(2, 4)]]
+            })
+        ]
+    })
 })
 
 const bar = () => Widget.Window ({
@@ -66,9 +129,9 @@ const bar = () => Widget.Window ({
     child: Widget.CenterBox ({
         className: "bar-content",
         vertical: true,
-        spacing: 300,
+        spacing: 100,
 
-        startWidget: Widget.Button ({}),
+        startWidget: launcher(),
         centerWidget: workspaces(),
         endWidget: quickView(),
     })
